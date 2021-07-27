4d31381a6d92730734cc889ec98f7883772a1c35
chosm7489 <chosm7489@gmail.com>
2021-07-27 17:12:46 -0400

    add --rm

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,6 +16,6 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         #test the email for a new user is test_new_user_email_normalized
         email = 'test@LONDONAPPDEV.COM'
-        user = get_user_model().objects.creat_user(email, 'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
